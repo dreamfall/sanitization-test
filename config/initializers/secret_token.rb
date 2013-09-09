efa562f204116ccc1be93b873a14d0866264bdec
dreamfall <amarant.st@gmail.com>
2013-09-09 17:26:39 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SanitizationTest::Application.config.secret_token = 'd46a185e547be576ad23fc521fe25410383c169789fcad0b5e45c9225c0d64b10217740b30e264dac8ac7437020ebf37d59412243ae8f23841485989ab0f281d'
